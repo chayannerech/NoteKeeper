e24fee0200fe8c2b04609295ee337c85da7d61b7
Chayanne Rech <rechchayanne@gmail.com>
2024-10-15 17:12:38 -0300

    Adicionado o botão de arquivar e a rota da ação

--- src/app/views/notas/notas.routes.ts
+++ src/app/views/notas/notas.routes.ts
@@ -3,11 +3,13 @@ import { ListarNotassComponent } from "./listar/listar-nota.component";
 import { InserirNotaComponent } from "./inserir/inserir-nota.component";
 import { EditarNotaComponent } from "./editar/editar-nota.component";
 import { ExcluirNotaComponent } from "./excluir/excluir-nota.component";
+import { ArquivarNotasComponent } from "./arquivar/arquivar-notas.component";
 
 export const notasRoutes: Routes = [
   { path: '', redirectTo: 'listar', pathMatch: 'full'},
   { path: 'listar', component: ListarNotassComponent},
   { path: 'inserir', component: InserirNotaComponent},
   { path: 'editar/:id', component: EditarNotaComponent},
-  { path: 'excluir/:id', component: ExcluirNotaComponent}
+  { path: 'excluir/:id', component: ExcluirNotaComponent},
+  { path: 'arquivar/:id', component: ArquivarNotasComponent}
 ]
